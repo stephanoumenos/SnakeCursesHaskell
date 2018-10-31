1c3ea9801de38cf626fb60dac93d1f739702eb7d
stephanoumenos <s_dechecchi@hotmail.com>
2018-10-30 22:47:33 -0300

    Add func spawnFood to make sure new food position is valid

--- Snake.hs
+++ Snake.hs
@@ -12,18 +12,6 @@ data Map = Map Snake Food deriving (Show)
 makeNewSnake :: Int -> Int -> Snake
 makeNewSnake x y = Snake "UP" [(x, y), (x, y-1), (x, y-2)]
 
-initialMap :: Int -> Int -> Map
-initialMap x_size y_size = Map newSnake newFood
-    where newSnake = makeNewSnake (quot x_size 2) (quot y_size 2)
-          newFood = randomFood x_size y_size
-
-moveSnake :: Snake -> Snake
-moveSnake (Snake d c) = Snake d newCoordinates
-    where newCoordinates | d == "UP" =  [(fst $ head c, (snd $ head c) + 1)] ++ init c
-                         | d == "DOWN" =  [(fst $ head c, (snd $ head c) - 1)] ++ init c
-                         | d == "LEFT" =  [((fst $ head c)-1, snd $ head c)] ++ init c
-                         | d == "RIGHT" =  [((fst $ head c)+1, snd $ head c)] ++ init c
-
 getRand :: Int -> Int
 getRand max = unsafePerformIO $ randomRIO (0, max)
 
@@ -31,6 +19,24 @@ randomFood :: Int -> Int -> Food
 randomFood max_x max_y = Food randomX randomY
     where randomX = getRand max_x
           randomY = getRand max_y
+
+spawnFood :: Int -> Int -> Snake -> Food
+spawnFood max_x max_y (Snake d c) = if (foodInSnake newFood (Snake d c))
+                                     then spawnFood max_x max_y (Snake d c)
+                                     else newFood
+    where newFood = randomFood max_x max_y
+
+initialMap :: Int -> Int -> Map
+initialMap x_size y_size = Map newSnake newFood
+    where newSnake = makeNewSnake (quot x_size 2) (quot y_size 2)
+          newFood = spawnFood x_size y_size newSnake
+
+moveSnake :: Snake -> Snake
+moveSnake (Snake d c) = Snake d newCoordinates
+    where newCoordinates | d == "UP" =  [(fst $ head c, (snd $ head c) + 1)] ++ init c
+                         | d == "DOWN" =  [(fst $ head c, (snd $ head c) - 1)] ++ init c
+                         | d == "LEFT" =  [((fst $ head c)-1, snd $ head c)] ++ init c
+                         | d == "RIGHT" =  [((fst $ head c)+1, snd $ head c)] ++ init c
 
 foodInSnake :: Food -> Snake -> Bool
 foodInSnake (Food x y) (Snake _ c) = elem (x, y) c
